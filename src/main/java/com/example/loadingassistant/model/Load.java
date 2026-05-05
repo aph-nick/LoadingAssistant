6259cf2fa42fce57dc034835893ebca8ee9dbc11
nick <hvnterz@gmail.com>
2026-05-05 21:58:35 +0200

    added project structure

--- src/main/java/com/example/loadingassistant/model/Load.java
+++ src/main/java/com/example/loadingassistant/model/Load.java
@@ -1,7 +1,19 @@
 package com.example.loadingassistant.model;
 
+import com.example.loadingassistant.model.util.Dimension;
+
 public class Load {
-    private int id;
-    private int length;
-    private int width;
+    private Dimension dimension;
+
+    public Load(Dimension dimension) {
+        this.dimension = dimension;
+    }
+
+    public Dimension getDimension() {
+        return dimension;
+    }
+
+    public void setDimension(Dimension dimension) {
+        this.dimension = dimension;
+    }
 }
