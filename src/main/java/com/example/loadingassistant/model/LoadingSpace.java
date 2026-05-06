12afdf59be868f6db6c3bb04a9a3daa7ce85e889
nick <hvnterz@gmail.com>
2026-05-06 19:56:26 +0200

    .

---
+++ src/main/java/com/example/loadingassistant/model/LoadingSpace.java
@@ -0,0 +1,11 @@
+package com.example.loadingassistant.model;
+
+import com.example.loadingassistant.model.util.Dimension;
+
+public class LoadingSpace {
+    private Dimension dimension;
+
+    public Dimension getDimension() {
+        return dimension;
+    }
+}
