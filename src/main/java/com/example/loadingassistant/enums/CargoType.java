3c30711dd2d8d7aec2cc5deef413b1ee8e5f19e2
nick <hvnterz@gmail.com>
2026-05-08 22:21:43 +0200

    added types for cargo and container

--- src/main/java/com/example/loadingassistant/enums/CargoType.java
+++ src/main/java/com/example/loadingassistant/enums/CargoType.java
@@ -1,8 +1,22 @@
 package com.example.loadingassistant.enums;
 
-public enum CargoType {
-    EuroPallet,
-    HalfPallet,
-    Custom,
-}
+import lombok.Getter;
 
+@Getter
+public enum CargoType {
+    EURO_PALLET(120.0, 80.0, 144.0, "Euro Pallet (EPAL)"),
+    HALF_PALLET(60.0, 80.0, 144.0, "Half Pallet"),
+    CUSTOM(0.0, 0.0, 0.0, "Custom Dimensions");
+
+    private final double length;
+    private final double width;
+    private final double height;
+    private final String displayName;
+
+    CargoType(double length, double width, double height, String displayName) {
+        this.length = length;
+        this.width = width;
+        this.height = height;
+        this.displayName = displayName;
+    }
+}
